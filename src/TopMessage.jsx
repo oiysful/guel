7ff5ff2ab71c38d525470a36d04b6dcf81d07500
ian <oiysful@gmai.com>
2025-10-24 16:16:51 +0900

    :construction: TopMessage, MainPhoto 추가

---
+++ src/TopMessage.jsx
@@ -0,0 +1,12 @@
+const TopMessage = () => {
+    return (
+        <>
+            <p style={{marginTop: "20px", marginBottom: "50px", fontSize: "20px", fontFamily: "MaruBuri"}}>
+                신랑님 & 신부님
+            </p>
+            <p>2222년 2월 22일 일요일 22시 22분</p>
+            <p>지옥불웨딩홀</p>
+        </>
+    )
+}
+export default TopMessage;
